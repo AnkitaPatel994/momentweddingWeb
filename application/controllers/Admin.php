0a071ec0280108aae3ca23f07fff1edd70f14647
Rupal <webservices.intelliworkz@gmail.com>
2017-10-06 12:19:37 +0530

    wedding controller

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,9 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
-
-	
+class Admin extends CI_Controller {	
 	public function index()
 	{		
 		$headerData = array(
@@ -23,5 +21,13 @@ class Admin extends CI_Controller {
 	}
 	public function login()
 	{	
+		$data=$_POST["data"];
+		$this->load->model("admin_model");
+		$result=$this->admin_model->doLogin($data);
+		var_dump($result);
 	}
+	 public function Wedding(){
+	 	
+	 }
+
 }
--- html/js/admin.js
+++ html/js/admin.js
@@ -1,3 +1,83 @@
 $(function(){
+	var baseURL=$("#base_url").val();
+	$('.modal').modal('open');
+
+	/*====================Login========================================*/
+	var data={
+		"email":$("#email").val(),
+		"password":$("#password").val()
+	}
+	$.post(baseURL+"Admin/login/",{data:data},function(data){
+		var data=$.parseJSON(data);
+		if(data.status=="ok"){
+			alert("LOgin Successfully....");
+			window.location.href=baseURL;
+		}
+		if(data.status=="fail"){
+			alert("Login Fail....");
+			window.location.href="#!";
+		}
+		else{
+			console.log(data);
+		}
+
+	});
+
+	/*====================Wedding========================================*/
+
+	$("#addWedding").on("click",function(){
+		$("#weddingModal").model('open');
+	});
+	/*===Add Wedding=======*/
+
+	$("#sendWeddingData").on("click",function(){
+		var formData = new FormData($("#addWeddingForm")[0]);
+		$.ajax({
+			data:formData,
+			url:"Admin/addWedding/",
+			type:"POST",
+			contentType:false,
+			processData:false,
+			success:function(result){
+				alert("Wedding added Successfully...");
+				window.location.reload();
+			}
+
+		});
+	});
+	/*===Update Wedding=======*/
+
+	$("#updateWeddingData").on("click",function(){
+		var formData = new FormData($("#updateWeddingForm")[0]);
+		$.ajax({
+			data:formData,
+			url:"Admin/updateWedding/",
+			type:"POST",
+			contentType:false,
+			processData:false,
+			success:function(result){
+				alert("Wedding Update Successfully...");
+				window.location.reload();
+			}
+
+		});
+	});
+
+	/*===Edit Wedding=======*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
