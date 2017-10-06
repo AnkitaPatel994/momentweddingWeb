fc5033ea7dcfdac4166e3022e115ad411f5ae489
Ravi <rmeswaniya@gmail.com>
2017-10-06 19:23:57 +0530

    Gallery Pge

--- application/views/eventsList.php
+++ application/views/eventsList.php
@@ -17,7 +17,7 @@
 			              <th>Date</th>
 			              <th>Time</th>
 			              <th>Location</th>
-			              <th>Action</th>
+			              <th colspan="3">Action</th>
 			          </tr>
 			        </thead>
 
@@ -32,8 +32,9 @@
 			            <td>06:00 pm</td>
 			            <td><textarea>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur, neque in laoreet convallis, sem quam semper justo, vitae varius purus turpis vel purus. Quisque venenatis metus vel ex egestas pretium.
 			            </textarea></td>
-			            <td><a href="#editProfile" class="btn-flat btn-edit waves-effect waves-light btn modal-trigger"><i class="material-icons">edit</i></td>
-			            <td><a href="#editProfile" class="btn-flat btn-delete waves-effect waves-light btn modal-trigger"><i class="material-icons">delete</i></td>
+			            <td><a href="#EditeventList" class="btn-flat btn-edit waves-effect waves-light btn modal-trigger"><i class="material-icons">edit</i></a>
+			            <a href="#EditeventGallery" class="btn-flat btn-gallery waves-effect waves-light btn modal-trigger"><i class="material-icons">burst_mode</i></a>	
+			            <a href="#editProfile" class="btn-flat btn-delete waves-effect waves-light btn modal-trigger"><i class="material-icons">delete</i></a></td>
 			          </tr>
 			          <tr>
 			            <td>1</td>
@@ -45,8 +46,9 @@
 			            <td>06:00 pm</td>
 			            <td><textarea>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur, neque in laoreet convallis, sem quam semper justo, vitae varius purus turpis vel purus. Quisque venenatis metus vel ex egestas pretium.
 			            </textarea></td>
-			            <td><a href="#editProfile" class="btn-flat btn-edit waves-effect waves-light btn modal-trigger"><i class="material-icons">edit</i></td>
-			            <td><a href="#editProfile" class="btn-flat btn-delete waves-effect waves-light btn modal-trigger"><i class="material-icons">delete</i></td>
+			            <td><a href="#EditeventList" class="btn-flat btn-edit waves-effect waves-light btn modal-trigger"><i class="material-icons">edit</i></a>
+			            <a href="#EditeventGallery" class="btn-flat btn-gallery waves-effect waves-light btn modal-trigger"><i class="material-icons">burst_mode</i></a>
+			            <a href="#editProfile" class="btn-flat btn-delete waves-effect waves-light btn modal-trigger"><i class="material-icons">delete</i></a></td>
 			          </tr>
 			        </tbody>
 			    </table>
@@ -105,6 +107,108 @@
 	        	</center>
 	        </div> -->
       	</form>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Add Event</a>
+    </div>
+  </div>
+
+
+  <!-- Edit Events -->
+
+
+  <!-- Modal Structure -->
+  <div id="EditeventList" class="modal modal-fixed-footer">
+    <div class="modal-content">
+      <h4>Events List</h4>
+      <!-- <p>A bunch of text</p> -->
+      <div class="profile-form row">
+      	<form>
+      		<div class="input-field col s12 m6" id="eventImage">
+	           <input type="file" name="eventImage" style="display:none;">
+          	   <button onclick="$('#eventImage input').click()" class="btn black"> Event Image</button>
+	        </div>
+      		<div class="input-field col s12 m6">
+	          <select>
+			      <option value="" disabled selected>Choose your option</option>
+			      <option value="1">Option 1</option>
+			      <option value="2">Option 2</option>
+			      <option value="3">Option 3</option>
+		      </select>
+	          <label for="name"> Name</label>
+	        </div>
+	        <div class="input-field col s12 m4">
+	        	<input type="text" class="validate" required="required">
+	          	<label for="event_name">Event Name</label>
+	        </div>
+	        <div class="input-field col s12 m4">
+	          <input type="text" class="datepicker">
+	          <label for="date">Date</label>
+	        </div>
+	        <div class="input-field col s12 m4">
+	          <input type="text" class="timepicker validate">
+	          <label for="time">Time</label>
+	        </div>
+	        <div class="input-field col s12 m12">
+	           <textarea id="textarea1" class="materialize-textarea"></textarea>
+          	   <label for="textarea1">Location</label>
+	        </div>
+	        <!-- <div class="input-field col s12 m12">
+	        	<center>
+	        		<button class="btn black"> Add Event</button>
+	        	</center>
+	        </div> -->
+      	</form>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Add Event</a>
+    </div>
+  </div>
+
+
+
+  <!-- Edit Event Gallery -->
+
+   <div id="EditeventGallery" class="modal modal-fixed-footer">
+    <div class="modal-content">
+      <h4>Events List</h4>
+      <!-- <p>A bunch of text</p> -->
+      <div class="profile-form row">
+      	<form>
+      		<div class="file-field input-field col s12">
+		      <div class="btn">
+		        <span>File</span>
+		        <input type="file" multiple>
+		      </div>
+		      <div class="file-path-wrapper">
+		        <input class="file-path validate" type="text" placeholder="Upload one or more files">
+		      </div>
+		    </div>
+
+      		<div class="input-field col s12" id="eventImage">
+	           <button class="btn black">Upload Even Images</button>
+	        </div>
+      	</form>
+      </div>
+
+      <div class="row">
+      	<div class="col s6 m4 eventImage-box">
+      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
+      	</div>
+      	<div class="col s6 m4 eventImage-box">
+      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
+      	</div>
+      	<div class="col s6 m4 eventImage-box">
+      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
+      	</div>
+      	<div class="col s6 m4 eventImage-box">
+      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
+      	</div>
+      	<div class="col s6 m4 eventImage-box">
+      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
+      	</div>
       </div>
     </div>
     <div class="modal-footer">
