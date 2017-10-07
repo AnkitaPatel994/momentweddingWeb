ab3bb927e4674803a3cfa75cc1ea496455c993e7
Ravi <rmeswaniya@gmail.com>
2017-10-07 17:09:24 +0530

    Guest List

--- application/views/guest_list.php
+++ application/views/guest_list.php
@@ -49,8 +49,7 @@
 			            <td>Mr. Guest</td>
 			            <td>7600278055</td>
 			            <td>Sangeet,Mahendi</td>
-			            <td><a href="#EditeventList" class="btn-flat btn-edit waves-effect waves-light btn modal-trigger"><i class="material-icons">edit</i></a>
-			            <a href="#EditeventGallery" class="btn-flat btn-gallery waves-effect waves-light btn modal-trigger"><i class="material-icons">burst_mode</i></a>	
+			            <td><a href="#EditGuestList" class="btn-flat btn-edit waves-effect waves-light btn modal-trigger"><i class="material-icons">edit</i></a>
 			            <a href="#editProfile" class="btn-flat btn-delete waves-effect waves-light btn modal-trigger"><i class="material-icons">delete</i></a></td>
 			          </tr>
 
@@ -81,76 +80,10 @@
   </div>
 
 
-  <!-- Edit Events -->
-
-
-  <!-- Modal Structure -->
-  <div id="EditeventList" class="modal modal-fixed-footer">
-    <div class="modal-content">
-      <h4>Events List</h4>
-      <!-- <p>A bunch of text</p> -->
-      
-    </div>
-    <div class="modal-footer">
-      <a href="#!" id="updateEventData" class="modal-action modal-close waves-effect waves-green btn-flat ">Update Event</a>
-    </div>
-  </div>
-
-
-
-  <!-- Edit Event Gallery -->
-
-   <div id="EditeventGallery" class="modal modal-fixed-footer">
-    <div class="modal-content">
-      <h4>Events List</h4>
-      <!-- <p>A bunch of text</p> -->
-      <div class="profile-form row">
-      	<form>
-      		<div class="file-field input-field col s12">
-		      <div class="btn">
-		        <span>File</span>
-		        <input type="file" multiple>
-		      </div>
-		      <div class="file-path-wrapper">
-		        <input class="file-path validate" type="text" placeholder="Upload one or more files">
-		      </div>
-		    </div>
-
-      		<div class="input-field col s12" id="eventImage">
-	           <button class="btn black">Upload Even Images</button>
-	        </div>
-      	</form>
-      </div>
-
-      <div class="row">
-      	<div class="col s6 m4 eventImage-box">
-      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
-      	</div>
-      	<div class="col s6 m4 eventImage-box">
-      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
-      	</div>
-      	<div class="col s6 m4 eventImage-box">
-      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
-      	</div>
-      	<div class="col s6 m4 eventImage-box">
-      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
-      	</div>
-      	<div class="col s6 m4 eventImage-box">
-      		<img src="<?php echo base_url(); ?>/html/images/profile.png">
-      	</div>
-      </div>
-    </div>
-    <div class="modal-footer">
-    </div>
-</div>
-
-
-
-
-
 
 <!-- Modal GUETS add -->
 <!-- Modal Structure -->
+
   <div id="addGuest" class="modal modal-fixed-footer">
     <div class="modal-content">
     	<div class="row">
@@ -178,12 +111,70 @@
 			    <label>Wedding</label>
 			</div>
 
-            <div class="input-field col s6 m4">
+            <div class="input-field col s12 m4">
 	          <input id="guest_name" type="text" class="validate" required="required">
 	          <label for="guest_name">Guest Name</label>
 	        </div>
 
-	        <div class="input-field col s6 m6">
+	        <div class="input-field col s12 m6">
+	          <input id="guest_mobile" type="text" class="validate" required="required">
+	          <label for="guest_mobile">Guest Mobile</label>
+	        </div>
+
+	        <div class="input-field col s12 m6">
+			    <select multiple>
+			      <option value="" disabled selected>Select Events</option>
+			      <option value="1">Sangeet</option>
+			      <option value="2">Mahendi</option>
+			    </select>
+			    <label>Side</label>
+            </div>
+
+      	</form>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Add New Guest</a>
+    </div>
+  </div>
+
+
+
+  <!-- Edit guest list -->
+
+<div id="EditGuestList" class="modal modal-fixed-footer">
+    <div class="modal-content">
+    	<div class="row">
+      		<center><h5>Edit New Guest</h5></center>
+      	</div>	
+      <!-- <p>A bunch of text</p> -->
+      <div class="guestList-area row">
+      	<form>
+      		<div class="input-field col s12 m4">
+			    <select>
+			      <option value="" disabled selected>Choose your option</option>
+			      <option value="1">Option 1</option>
+			      <option value="2">Option 2</option>
+			      <option value="3">Option 3</option>
+			    </select>
+			    <label>Wedding</label>
+			</div>
+
+			<div class="input-field col s12 m4">
+			    <select>
+			      <option value="" disabled selected>Choose your option</option>
+			      <option value="1">Groom</option>
+			      <option value="2">Bride</option>
+			    </select>
+			    <label>Wedding</label>
+			</div>
+
+            <div class="input-field col s12 m4">
+	          <input id="guest_name" type="text" class="validate" required="required">
+	          <label for="guest_name">Guest Name</label>
+	        </div>
+
+	        <div class="input-field col s12 m6">
 	          <input id="guest_mobile" type="text" class="validate" required="required">
 	          <label for="guest_mobile">Guest Mobile</label>
 	        </div>
