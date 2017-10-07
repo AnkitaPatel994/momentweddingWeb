3bcc413581d1f91c190bc9df6cb47ea97547e481
Ravi <rmeswaniya@gmail.com>
2017-10-07 17:31:35 +0530

    Wedding Dashboard Btn chnges

--- application/views/weddingDashboard.php
+++ application/views/weddingDashboard.php
@@ -1,3 +1,33 @@
+<style type="text/css">
+  
+.btn-edit-wed{
+    background: #eee;
+    color: #000;
+    border: 1px solid #ddd;
+    border-radius: 69px;
+    padding: 0px 7px;
+    margin: 0 auto;
+    text-align: center;
+}
+.btn-edit-wed:hover
+{
+    background:rgba(0,0,0,0.2) !important;
+}
+
+.btn-delete-wed {
+    background: rgba(222, 21, 21, 0.20);
+    border: 1px solid #e69b9b;
+    color: #000;
+    border-radius: 69px;
+    padding: 0px 7px;
+    margin: 0 auto;
+    text-align: center;
+}
+.btn-delete-wed:hover {
+    background: rgba(222, 21, 21, 0.50) !important;
+}
+</style>
+
 <div class="row top-header">
   <div class="col m2">
     <div class="sidebar-area sticky">
@@ -22,7 +52,7 @@
     <div class="row">
       <div class="page-title col s6 m10">Wedding Management</div>      
       <div class="page-button col s6 m2">
-        <a class="waves-effect waves-light btn black modal-trigger" href="#weddingModal">Add</a>
+        <a class="waves-effect waves-light btn black modal-trigger" href="#weddingModal">Add Wedding</a>
       </div>
     </div>  
   </div>
@@ -50,10 +80,8 @@
         <td><?php echo $allWeddingRow['code']; ?></td>        
       
         <td class="right-align blog-btn">
-        <a data-wed-id="<?php echo $allWeddingRow['id']; ?>" class="btn-floating waves-effect waves-light red delete-wedding"><i class="material-icons">delete</i></a>
-        <a data-wed-id="<?php echo $allWeddingRow['id']; ?>" class="btn-floating waves-effect waves-light blue-grey edit-wedding"><i class="material-icons">mode_edit</i></a>
-
-        </td>
+        <a data-wed-id="<?php echo $allWeddingRow['id']; ?>" class="btn btn-edit-wed edit-wedding"><i class="material-icons">mode_edit</i></a>
+        <a data-wed-id="<?php echo $allWeddingRow['id']; ?>" class="btn btn-delete-wed delete-wedding"><i class="material-icons">delete</i></a></td>
       </tr>
       <?php } ?>
       </tbody>
